0e5c8d563bbc289780fb08be308390ccf3ac4c88
msyaifullahworks <muhammad.syaifullah@kodingworks.io>
2022-08-18 00:56:36 +0700

    final-dev

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -21,14 +24,18 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
+      terser(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
--- src/components/SaeToast/SaeToast.tsx
+++ src/components/SaeToast/SaeToast.tsx
@@ -1,27 +1,42 @@
-import React from "react";
-// import "./SaeToast.css";
+import React, { useEffect, useState } from "react";
+import "./SaeToast.css";
 
 interface ToastProps {
   message: string;
   open: boolean;
   type?: string;
+  position?: string;
 }
 
 export default function SaeToast(props: ToastProps) {
-  const { message, open, type } = props;
+  const { message, open, type, position } = props;
+  const [show, setShow] = useState(open);
+
+  useEffect(() => {
+    setShow(open);
+  }, [open]);
+
+  useEffect(() => {
+    if (open) {
+      const hide = setTimeout(() => {
+        setShow(false);
+      }, 5000);
+      return () => clearTimeout(hide);
+    }
+  }, [show]);
 
   return (
     <div>
       {open && (
-        <div className="wrapper">
-          <div>
-            {type === "success" ? (
-              <img src="" />
-            ) : type === "error" ? (
-              <img src="" />
-            ) : null}
+        <div className={`wrapper ${position || "top-right"}`}>
+          {type === "success" ? (
+            <img src="/success.svg" width={25} height={25} />
+          ) : type === "error" ? (
+            <img src="/error.svg" width={25} height={25} />
+          ) : null}
+          <div style={{ width: !type ? "100%" : "90%" }} className="message">
+            {message}
           </div>
-          <div style={{ marginLeft: !type ? 0 : 16 }}>{message}</div>
         </div>
       )}
     </div>
